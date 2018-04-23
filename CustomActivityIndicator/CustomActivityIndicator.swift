729ac569837a3480e5e1055584e25b848ac6d55b
Dave <Dave.Troupe@gmail.com>
2018-04-23 13:44:44 -0400

    Added support to embed a label in the activity indicator.

--- CustomActivityIndicator/CustomActivityIndicator.swift
+++ CustomActivityIndicator/CustomActivityIndicator.swift
@@ -18,42 +18,18 @@ class CustomActivityIndicator {
     
     var loadingView: UIView = UIView()
     var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    var label: UILabel = UILabel()
     
     // Show customized activity indicator,
     // actually add activity indicator to passing view
     // @param uiView - add activity indicator to this view
+    // @param color (optional) - color for the background of the activity indicator
     //
-    func showActivityIndicator(uiView: UIView) {
+    func showActivityIndicator(uiView: UIView, color: UIColor?) {
         uiView.isUserInteractionEnabled = false
         loadingView.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
         loadingView.center = uiView.center
-        loadingView.backgroundColor = UIColor.lightGray
-        loadingView.clipsToBounds = true
-        loadingView.layer.cornerRadius = 10
-        
-        activityIndicator.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
-        activityIndicator.activityIndicatorViewStyle =
-            UIActivityIndicatorViewStyle.whiteLarge
-        activityIndicator.center = CGPoint(x: loadingView.frame.size.width / 2, y: loadingView.frame.size.height / 2)
-        activityIndicator.hidesWhenStopped = true
-        
-        DispatchQueue.main.async {
-            self.loadingView.addSubview(self.activityIndicator)
-            uiView.addSubview(self.loadingView)
-            self.activityIndicator.startAnimating()
-        }
-    }
-    
-    // Show customized activity indicator,
-    // actually add activity indicator to passing view
-    // @param uiView - add activity indicator to this view
-    // @param color - color for the background of the activity indicator
-    //
-    func showActivityIndicator(uiView: UIView, color: UIColor) {
-        uiView.isUserInteractionEnabled = false
-        loadingView.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
-        loadingView.center = uiView.center
-        loadingView.backgroundColor = color
+        loadingView.backgroundColor = color ?? UIColor.darkGray
         loadingView.clipsToBounds = true
         loadingView.layer.cornerRadius = 10
         
@@ -76,11 +52,11 @@ class CustomActivityIndicator {
     // @param color - color for the background of the activity indicator
     // @param size - desired size of the activity indicator
     //
-    func showActivityIndicator(uiView: UIView, color: UIColor, size: Double) {
+    func showActivityIndicator(uiView: UIView, color: UIColor?, size: Double) {
         uiView.isUserInteractionEnabled = false
         loadingView.frame = CGRect(x: 0, y: 0, width: size, height: size)
         loadingView.center = uiView.center
-        loadingView.backgroundColor = color
+        loadingView.backgroundColor = color ?? UIColor.darkGray
         loadingView.clipsToBounds = true
         loadingView.layer.cornerRadius = 10
         
@@ -96,6 +72,60 @@ class CustomActivityIndicator {
             self.activityIndicator.startAnimating()
         }
     }
+    
+    // Show customized activity indicator,
+    // actually add activity indicator to passing view
+    // @param uiView - add activity indicator to this view
+    // @param color (optional) - color for the background of the activity indicator
+    // @param size - desired size of the activity indicator
+    //
+    func showActivityIndicator(uiView: UIView, color: UIColor?, labelText: String) {
+        uiView.isUserInteractionEnabled = false
+        
+        let width: CGFloat = labelText.width(withConstrainedHeight: 21.0, font: UIFont.systemFont(ofSize: 17))
+        
+        // Max width is 200 if width is greater than this we need to create a multi-lined label
+        //
+        if width > 200 {
+            let height: CGFloat = labelText.height(withConstrainedWidth: 200, font: UIFont.systemFont(ofSize: 17))
+            loadingView.frame = CGRect(x: 0, y: 0, width: 220, height: 100 + height)
+            loadingView.center = uiView.center
+            label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: height))
+            label.center = CGPoint(x: loadingView.frame.size.width / 2, y: 50 + height)
+            label.numberOfLines = 0
+            activityIndicator.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+            activityIndicator.center = CGPoint(x: 110, y: 50)
+            
+        } else {
+            loadingView.frame = CGRect(x: 0, y: 0, width: width + 20, height: 100)
+            loadingView.center = uiView.center
+            label = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: 21))
+            label.center = CGPoint(x: loadingView.frame.size.width / 2, y: 90)
+            activityIndicator.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+            activityIndicator.center = CGPoint(x: loadingView.frame.size.width / 2, y: loadingView.frame.size.height / 2)
+        }
+        
+        
+        loadingView.backgroundColor = color ?? UIColor.darkGray
+        loadingView.clipsToBounds = true
+        loadingView.layer.cornerRadius = 10
+        
+        label.textAlignment = .center
+        label.textColor = UIColor.white
+        label.text = labelText
+        
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.whiteLarge
+        activityIndicator.hidesWhenStopped = true
+        
+        DispatchQueue.main.async {
+            self.loadingView.addSubview(self.label)
+            self.loadingView.addSubview(self.activityIndicator)
+            uiView.addSubview(self.loadingView)
+            self.activityIndicator.startAnimating()
+        }
+    }
+    
+    
     
     // Hide activity indicator
     // Actually remove activity indicator from its super view
