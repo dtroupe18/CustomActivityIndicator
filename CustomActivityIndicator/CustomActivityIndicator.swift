972ee443b06ccf96b0a1575127ad32c048ed128a
Dave <Dave.Troupe@gmail.com>
2018-04-23 13:46:41 -0400

    Updates to support embedding a label in the activity indicator

--- CustomActivityIndicator/CustomActivityIndicator.swift
+++ CustomActivityIndicator/CustomActivityIndicator.swift
@@ -11,7 +11,7 @@ import UIKit
 
 class CustomActivityIndicator {
     
-    // we need to make this a singleton in order for
+    // We need to make this a singleton in order for
     // the same views to be added and removed each time
     //
     static let sharedInstance = CustomActivityIndicator()
@@ -19,6 +19,31 @@ class CustomActivityIndicator {
     var loadingView: UIView = UIView()
     var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
     var label: UILabel = UILabel()
+    
+    // Show customized activity indicator,
+    // actually add activity indicator to passing view
+    // @param uiView - add activity indicator to this view
+    //
+    func showActivityIndicator(uiView: UIView) {
+        uiView.isUserInteractionEnabled = false
+        loadingView.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
+        loadingView.center = uiView.center
+        loadingView.backgroundColor = UIColor.lightGray
+        loadingView.clipsToBounds = true
+        loadingView.layer.cornerRadius = 10
+        
+        activityIndicator.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+        activityIndicator.activityIndicatorViewStyle =
+            UIActivityIndicatorViewStyle.whiteLarge
+        activityIndicator.center = CGPoint(x: loadingView.frame.size.width / 2, y: loadingView.frame.size.height / 2)
+        activityIndicator.hidesWhenStopped = true
+        
+        DispatchQueue.main.async {
+            self.loadingView.addSubview(self.activityIndicator)
+            uiView.addSubview(self.loadingView)
+            self.activityIndicator.startAnimating()
+        }
+    }
     
     // Show customized activity indicator,
     // actually add activity indicator to passing view
@@ -105,7 +130,6 @@ class CustomActivityIndicator {
             activityIndicator.center = CGPoint(x: loadingView.frame.size.width / 2, y: loadingView.frame.size.height / 2)
         }
         
-        
         loadingView.backgroundColor = color ?? UIColor.darkGray
         loadingView.clipsToBounds = true
         loadingView.layer.cornerRadius = 10
@@ -124,8 +148,6 @@ class CustomActivityIndicator {
             self.activityIndicator.startAnimating()
         }
     }
-    
-    
     
     // Hide activity indicator
     // Actually remove activity indicator from its super view
@@ -157,8 +179,6 @@ class CustomActivityIndicator {
             }
         }
     }
-    
-    
     
     // Create UIColor from hex values
     // @param rgbValue - hex color value
