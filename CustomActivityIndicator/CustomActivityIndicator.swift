5145a1729c217df8a552e329561f28ac074327f5
Dave <Dave.Troupe@gmail.com>
2018-05-08 15:43:08 -0400

    Moved required extensions into CustomActivityIndicator Class

--- CustomActivityIndicator/CustomActivityIndicator.swift
+++ CustomActivityIndicator/CustomActivityIndicator.swift
@@ -207,5 +207,21 @@ class CustomActivityIndicator {
 // In order to hide the activity indicator, call the function from your view controller
 // CustomActivityIndicator.sharedInstance.hideActivityIndicator(uiView: self.view)
 
+extension String {
+    
+    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font: font], context: nil)
+        
+        return ceil(boundingBox.height)
+    }
+    
+    func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font: font], context: nil)
+        
+        return ceil(boundingBox.width)
+    }
+}
 
 
